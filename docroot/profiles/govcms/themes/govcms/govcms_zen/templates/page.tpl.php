2b3a320b8afc150592153e132bb3a15ae85b583c
Laura Higgins <laura.higgins@sa.gov.au>
2015-08-14 13:13:19 +0930

    Committed by DevDesktop

--- docroot/profiles/govcms/themes/govcms/govcms_zen/templates/page.tpl.php
+++ docroot/profiles/govcms/themes/govcms/govcms_zen/templates/page.tpl.php
@@ -8,6 +8,7 @@
  */
 ?>
 
+<div class="layout-centered page-wrapper">
 <header class="header" id="header" role="banner">
   <div class="header__inner">
 
@@ -35,6 +36,20 @@
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
   <?php endif; ?>
 
+  <?php if ($site_name || $site_slogan): ?>
+  <div class="header__name-and-slogan" id="name-and-slogan">
+    <?php if ($site_name): ?>
+      <h1 class="header__site-name" id="site-name">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
+      </h1>
+    <?php endif; ?>
+
+    <?php if ($site_slogan): ?>
+      <h2 class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></h2>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>
+  
   <?php print render($page['header']); ?>
   </div>
 </header>
@@ -89,3 +104,5 @@
 </div>
 
 <?php print render($page['bottom']); ?>
+
+</div>
